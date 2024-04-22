c44f40ae2427f985275f6716eb0cd7366715bdec
Naga Malleswari Chennamsetti <60144643+nagamalli148@users.noreply.github.com>
2024-04-22 11:42:15 +0530

    Add files via upload

---
+++ src/components/NewsBoard.jsx
@@ -0,0 +1,30 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import Newsitem from "./Newsitem";
+
+const NewsBoard = () => {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    let url =
+      "https://newsapi.org/v2/top-headlines?country=us&apiKey=${import.meta.env.VITE_API_KEY}";
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setArticles(data.articles));
+  }, []);
+
+  return (
+    <div>
+      <h2 className="text-center">Latest <span className="badge bg-danger"> News </span> </h2>
+        
+    {articles.map(news,index) =>
+        {
+          return <Newsitem key={index} title={news.title} description={news.description} src={news.urlToImage} url={news.url} />
+        }
+        }
+
+    </div>
+  );
+};
+
+export default NewsBoard;
